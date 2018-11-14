93211a067ea729aad6059be0344304a0cc53fb6a
gaohuan214 <18855972539@163.com>
2018-11-14 15:26:24 +0800

    重置表单,及后台验证功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -22,8 +22,12 @@
                     // 非空校验
                     notEmpty: {
                         message: "用户名不能为空"
+                    },
+                    callback: {
+                        message: "用户名不存在"
                     }
-                }
+                },
+
             },
             //验证密码
             password: {
@@ -36,6 +40,10 @@
                         min: 6,
                         max: 12,
                         message: "密码长度为6-12位"
+                    },
+
+                    callback: {
+                        message : "密码错误",
                     }
                 }
             }
@@ -53,10 +61,28 @@
             data: $('#form').serialize(),
             dataType: 'json',
             success: function( info){
-                //验证成功过，跳转到index页面
-                location.href = "index.html";
+                console.log(info);
+               // 参数1: 校验字段  username/password
+                // 参数2: 校验状态  NOT_VALIDATED(未校验), VALIDATING(校验中), INVALID(失败) or VALID(成功)
+                // 参数3: 校验规则, 用来配置错误时的提示信息
+                if(info.error === 1001){
+                    // 密码错误
+                    $('#form').data("bootstrapValidator").updateStatus("password","INVALID","callback");
+                }
+                else if(info.error === 1000 ){
+                   //   用户名不存在
+                    $('#form').data("bootstrapValidator").updateStatus("username","INVALID","callback");
+                }else if(info.success){
+                     // 表示验证成功
+                     location.href = "index.html";
+                }
             }
         })
     });
-
 })
+
+// 重置表单
+$('[type="reset"]').on("click",function(){
+
+    $('#form').data("bootstrapValidator").resetForm( );
+});
