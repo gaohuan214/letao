f6a1f747ec70658e65266be1c4ca417ad78a80f9
gaohuan214 <18855972539@163.com>
2018-11-15 23:07:47 +0800

    设置登录拦截

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,18 +1,19 @@
 
 // 功能1: 进度条功能
-;$(function(){
 
     // 全局ajax事件处理函数
     $(document).ajaxStart(function(){
         // 开启进度条
         NProgress.start();
-    })
+    });
 
     $(document).ajaxStop(function(){
-        // 结束进度条
-        NProgress.done();
+        setTimeout(function(){
+            // 结束进度条
+            NProgress.done();
+        },500)
     })
-});
+
 
 // 功能2: 侧边栏 点击分类管理部分显示隐藏
 ;$(function(){
@@ -49,6 +50,23 @@
 //功能5: 登出功能
 // 点击登出按钮,向后台提交登出请求
 ;$(function(){
-    
-})
+    $('#logout').click(function(){
+
+        $.ajax({
+            type: "get",
+            url: "/employee/employeeLogout",
+            dataType: "json",
+            success: function( info ){
+                if( info.success){
+                    //成功 ,返回登录页面
+                    location.href = "login.html";
+                }else if( info.error){
+                    alert('登出失败,请稍后重试');
+                }
+            }
+        })
+    })
+});
+
+
 
--- public/back/js/isLogin.js
+++ public/back/js/isLogin.js
@@ -1,2 +1,16 @@
 
-//
+// 进行登录拦截,页面一加载就判断用户是否登录过,没有,去登录页面登录
+// 前端不知道是否登录过,但是后台指定,向后台请求数据
+;$(function(){
+    $.ajax({
+        type: "get",
+        url: "/employee/checkRootLogin",
+        dataType: 'json',
+        success: function( info){
+            console.log( info);
+            if( info.error === 400){
+                location.href = "login.html";
+            }
+        }
+    })
+})
